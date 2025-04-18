0684897840a25809124a8fd5184d96be0e5e2dfb
Marya Kruzhilina <musatowa.marya@yandex.ru>
2025-04-18 12:09:15 +0300

    final

--- src/main/java/kruzhilina_userLoc/Main.java
+++ src/main/java/kruzhilina_userLoc/Main.java
@@ -3,7 +3,7 @@ package kruzhilina_userLoc;
 public class Main {
     public static void main(String[] args) {
         Control control = new DamageControl();
-        //создаем имитацию событий дважды повышение damage и одно понижение
+        //создаем имитацию событий дважды повышение damage(+50) и одно понижение(-50) ожидаемый результат 150
         int finalDamage = new IncreasedControlDamageDecorator(new DecreaseControlDamageDecorator(new IncreasedControlDamageDecorator(control))).getDamage();
         System.out.println(finalDamage);
         //не изменяя основную реализацию DamageControl мы изменили результат через обёртки
